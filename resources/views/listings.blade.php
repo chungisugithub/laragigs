f92805c374741e215c7165c43e7e7e5dcc5a9ebe
chungisugithub <chungisu@gmail.com>
2022-10-22 04:06:59 +0900

    {{$listing->title}} eloquent model collection

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -17,7 +17,7 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             />
             <div>
                 <h3 class="text-2xl">
-                    <a href="show.html">{{$listing['title']}}</a>
+                    <a href="show.html">{{$listing->title}}</a>
                 </h3>
                 <div class="text-xl font-bold mb-4">Acme Corp</div>
                 <ul class="flex">
