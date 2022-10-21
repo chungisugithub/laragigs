8c39bd1fe733c14028568dd3218a9ec6b52e8469
chungisugithub <chungisu@gmail.com>
2022-10-21 16:47:33 +0900

    return $request->name . ' ' . $request->city;

--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,5 @@ Route::get('/posts/{id}', function($id) {
 })->where('id', '[0-9]+');
 
 Route::get('/search', function(Request $request) {
-    dd($request->name . ' ' . $request->city);
+    return $request->name . ' ' . $request->city;
 });
