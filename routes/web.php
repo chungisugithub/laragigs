efffb425505edf580a6d2d35e33e2720c46ed363
chungisugithub <chungisu@gmail.com>
2022-10-21 17:36:34 +0900

    remove all added routes

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function() {
-    return response('<h1>Hello World</h1>',200)
-        ->header('Content-Type', 'text/plain')
-        ->header('foo', 'bar');
-});
-
-Route::get('/posts/{id}', function($id) {
-    ddd($id);
-    return response('Post '. $id);
-})->where('id', '[0-9]+');
-
-Route::get('/search', function(Request $request) {
-    return $request->name . ' ' . $request->city;
-});
