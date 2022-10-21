9768e92674350e19b40084f9ef95772d87ef34db
chungisugithub <chungisu@gmail.com>
2022-10-21 19:50:08 +0900

    add find by id method in Listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -16,6 +16,16 @@ class Listing {
             ]
         ];
     }
+
+    public static function find($id) {
+        $listings = self::all();
+
+        foreach($listings as $listing) {
+            if($listing['id'] == $id) {
+                return $listing;
+            }
+        }
+    }
 }
 
 ?>
