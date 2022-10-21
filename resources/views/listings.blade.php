d721a1ec312304ec54400f1f287fbb8bfe56be11
chungisugithub <chungisu@gmail.com>
2022-10-21 18:56:13 +0900

    php to blade

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,5 @@
 <h1>{{$heading}}</h1>
 @foreach($listings as $listing)
-    <h2><?php echo $listing['title']; ?></h2>
-    <p><?php echo $listing['description']; ?></p>
+    <h2>{{$listing['title']}}</h2>
+    <p>{{$listing['description']}}</p>
 @endforeach
