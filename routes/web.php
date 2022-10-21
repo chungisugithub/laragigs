c219f22a2b8aa4e1eaccdc0a9df0cc57b9930317
chungisugithub <chungisu@gmail.com>
2022-10-21 19:50:41 +0900

    add Single Listing route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use App\Models\Listing;
 |
 */
 
+// All Listings
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'Latest Listings',
         'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
     ]);
 });
