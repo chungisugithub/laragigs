81443bb6a68479ce4fe6ca5a275523b2b525f4ac
chungisugithub <chungisu@gmail.com>
2022-10-22 15:15:17 +0900

    mailto:{{$listing->email}}

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -51,7 +51,7 @@
                 {{$listing->description}}
 
                 <a
-                    href="mailto:test@test.com"
+                    href="mailto:{{$listing->email}}"
                     class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
                     ><i class="fa-solid fa-envelope"></i>
                     Contact Employer</a
