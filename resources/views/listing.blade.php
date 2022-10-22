8cbc4f40a48a52d96db31608ff890707ace6c0f1
chungisugithub <chungisu@gmail.com>
2022-10-22 15:11:40 +0900

    오타 {{$listing->company}}

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -16,7 +16,7 @@
         />
 
         <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
-        <div class="text-xl font-bold mb-4">{{$listing-company}}</div>
+        <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
         <ul class="flex">
             <li
                 class="bg-black text-white rounded-xl px-3 py-1 mr-2"
@@ -40,7 +40,7 @@
             </li>
         </ul>
         <div class="text-lg my-4">
-            <i class="fa-solid fa-location-dot"></i> Daytona, FL
+            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
         </div>
         <div class="border border-gray-200 w-full mb-6"></div>
         <div>
