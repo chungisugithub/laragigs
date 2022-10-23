fd7c0f606300660a36a912f993e5e066cb698ef6
chungisugithub <chungisu@gmail.com>
2022-10-24 05:35:36 +0900

    add edit route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show Edit Data
+Route::get('/listings/{listing}/edit',[ListingController::class, 'edit']);
 
 
 // Single Listing
