2c901c04a01a1e20e001338c1e1e62ca9d1c7255
chungisugithub <chungisu@gmail.com>
2022-10-24 00:05:58 +0900

    Route::get('/listings/create'

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,5 @@ Route::get('/', [ListingController::class, 'index']);
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
