bbea4f49f16c1aaf51edacb10aa6c306280d56df
chungisugithub <chungisu@gmail.com>
2022-10-24 02:27:24 +0900

    데이터베이스에 저장

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -38,6 +38,9 @@ class ListingController extends Controller
             'description' => 'required'
         ]);
 
+        Listing::create($formFields);
+
+
         return redirect('/');
     }
 }
