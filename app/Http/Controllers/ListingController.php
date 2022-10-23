bb05b865aa00d35741f8c75fcf6608ef80e80dc9
chungisugithub <chungisu@gmail.com>
2022-10-24 04:37:12 +0900

    dd($request->file('logo')->store());

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -31,7 +31,7 @@ class ListingController extends Controller
 
     // Store Listing Data
     public function store(Request $request) {
-        dd($request->file('logo'));
+        dd($request->file('logo')->store());
         $formFields = $request->validate([
             'title' => 'required',
             'company' => ['required', Rule::unique('listings','company')] ,
