3314b0436986e5fd2542187b63a7afe405aec451
chungisugithub <chungisu@gmail.com>
2022-10-24 01:21:08 +0900

    파일은 다음에 주석처리

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -87,7 +87,7 @@ class="p-10 max-w-lg mx-auto mt-24"
         />
     </div>
 
-    <div class="mb-6">
+    {{-- <div class="mb-6">
         <label for="logo" class="inline-block text-lg mb-2">
             Company Logo
         </label>
@@ -96,7 +96,7 @@ class="p-10 max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="logo"
         />
-    </div>
+    </div> --}}
 
     <div class="mb-6">
         <label
