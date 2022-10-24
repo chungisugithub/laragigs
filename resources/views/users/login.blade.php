e7e935fa3607e4a248a8ec2ac40c9c1f8e24ed4d
chungisugithub <chungisu@gmail.com>
2022-10-24 17:16:39 +0900

    update login page

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,28 +3,13 @@
 
         <header class="text-center">
         <h2 class="text-2xl font-bold uppercase mb-1">
-            Register
+            Login
         </h2>
-        <p class="mb-4">Create an account to post gigs</p>
+        <p class="mb-4">Log into your account to post gigs</p>
     </header>
 
-    <form method="POST" action="/users">
+    <form method="POST" action="/users/login">
         @csrf
-        <div class="mb-6">
-            <label for="name" class="inline-block text-lg mb-2">
-                Name
-            </label>
-            <input
-                type="text"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="name"
-                value={{old('name')}}
-            >
-            @Error('name')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @endError
-        </div>
-
         <div class="mb-6">
             <label for="email" class="inline-block text-lg mb-2"
                 >Email</label
@@ -59,37 +44,19 @@
         </div>
 
         <div class="mb-6">
-            <label
-                for="password2"
-                class="inline-block text-lg mb-2"
-            >
-                Confirm Password
-            </label>
-            <input
-                type="password"
-                class="border border-gray-200 rounded p-2 w-full"
-                name="password_confirmation"
-                value={{old('password_confirmation')}}
-            >
-            @Error('password_confirmation')
-            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @endError
-        </div>
-
-        <div class="mb-6">
             <button
                 type="submit"
                 class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
             >
-                Sign Up
+                Sign In
             </button>
         </div>
 
         <div class="mt-8">
             <p>
-                Already have an account?
-                <a href="/login" class="text-laravel"
-                    >Login</a
+                Don't have an account?
+                <a href="/register" class="text-laravel"
+                    >Register</a
                 >
             </p>
         </div>
