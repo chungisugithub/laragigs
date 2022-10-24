952f9c2f2ee4b0cca725f34ba440822efa7d1dd0
chungisugithub <chungisu@gmail.com>
2022-10-24 14:10:43 +0900

    use App\Http\Controllers\UserController;

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
