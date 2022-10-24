f1fdebfbc0376f5604dfef63eeb7bb8eff3707ac
chungisugithub <chungisu@gmail.com>
2022-10-24 17:35:13 +0900

    fix typo

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -8,7 +8,7 @@
         <p class="mb-4">Log into your account to post gigs</p>
     </header>
 
-    <form method="POST" action="/users/autheticate">
+    <form method="POST" action="/users/authenticate">
         @csrf
         <div class="mb-6">
             <label for="email" class="inline-block text-lg mb-2"
