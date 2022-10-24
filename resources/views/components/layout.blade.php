b3fdadc1ce6ac2064070f6185eebc3c09ceb8e85
chungisugithub <chungisu@gmail.com>
2022-10-24 16:35:15 +0900

    add logout ui

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -45,6 +45,12 @@
                         Manage Listings</a
                     >
                 </li>
+                <li>
+                    <form class="inline" method="POST" action="/logout">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i>Logout
+                </li>
                 @else
                 <li>
                     <a href="/register" class="hover:text-laravel"
