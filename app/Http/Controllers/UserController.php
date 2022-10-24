82c28b7bcef78814a35665d08d952fbf7dc12cc5
chungisugithub <chungisu@gmail.com>
2022-10-24 16:49:07 +0900

    로그아웃 세션 정리 리다이렉트

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,5 +37,10 @@ class UserController extends Controller
     // Logout User
     public function logout(Request $request) {
         auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'You have been logged out!');
     }
 }
