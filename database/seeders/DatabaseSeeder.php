d23ed2da2f033fc329507b89df7ed247b00d61c3
chungisugithub <chungisu@gmail.com>
2022-11-18 17:12:33 +0900

    하나의 리스트 생성 Listing::create()

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+
+        Listing::create(
+            [
+                'title' => 'Laravel Senior Developer', 
+                'tags' => 'laravel, javascript',
+                'company' => 'Acme Corp',
+                'location' => 'Boston, MA',
+                'email' => 'email1@email.com',
+                'website' => 'https://www.acme.com',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+            ]
+        );
     }
 }
