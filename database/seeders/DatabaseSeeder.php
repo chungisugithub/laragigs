0b1ab5fdd275a537b5265fa5c0629e8a670fb20f
chungisugithub <chungisu@gmail.com>
2022-11-18 17:49:34 +0900

    샘플테이터 작성

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
         Listing::create(
             [
@@ -26,6 +26,52 @@ class DatabaseSeeder extends Seeder
                 'website' => 'https://www.acme.com',
                 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
             ]
+        );
+        Listing::create( 
+            [
+            'title' => 'Full-Stack Engineer',
+            'tags' => 'laravel, backend ,api',
+            'company' => 'Stark Industries',
+            'location' => 'New York, NY',
+            'email' => 'email2@email.com',
+            'website' => 'https://www.starkindustries.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+          ]
+        );
+        Listing::create(
+            [
+                'title' => 'Laravel Developer', 
+                'tags' => 'laravel, vue, javascript',
+                'company' => 'Wayne Enterprises',
+                'location' => 'Gotham, NY',
+                'email' => 'email3@email.com',
+                'website' => 'https://www.wayneenterprises.com',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+              ]
+        );
+
+        Listing::create(
+            [
+                'title' => 'Backend Developer', 
+                'tags' => 'laravel, php, api',
+                'company' => 'Skynet Systems',
+                'location' => 'Newark, NJ',
+                'email' => 'email4@email.com',
+                'website' => 'https://www.skynet.com',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+              ]
+        );        
+        
+        Listing::create(
+            [
+                'title' => 'Junior Developer', 
+                'tags' => 'laravel, php, javascript',
+                'company' => 'Wonka Industries',
+                'location' => 'Boston, MA',
+                'email' => 'email4@email.com',
+                'website' => 'https://www.wonka.com',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+              ]
         );
     }
 }
