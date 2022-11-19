becde3361fb08c17715bf9a55ab793954cb0af56
chungisugithub <chungisu@gmail.com>
2022-11-20 02:43:13 +0900

    카드 컴포넌트 만들기

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,7 +1,7 @@
 @props(['listing'])
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
-  <div class="flex">
+<x-card>
+  <div class="flex">  
     <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="">
     <div>
       <h3 class="text-2xl">
@@ -27,4 +27,4 @@
       </div>
     </div>
   </div>
-</div>
+</x-card>
