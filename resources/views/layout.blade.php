78d4d45d12b77f330f7dce2d1885f3fe3d17012d
chungisugithub <chungisu@gmail.com>
2022-11-19 15:53:03 +0900

    푸트고정으로 인해 바디 가림 처리<body class="mb-48">

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
     <title>LaraGigs | Find Laravel Jobs & Projects</title>
 </head>
 
-<body>
+<body class="mb-48">
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt=""></a>
         <ul class="flex space-x-6 mr-6 text-lg">
