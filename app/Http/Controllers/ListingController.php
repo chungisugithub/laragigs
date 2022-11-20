c5d22bc7f193552706b370dbe8620f0bc16defa9
chungisugithub <chungisu@gmail.com>
2022-11-21 03:11:35 +0900

    리스팅 컨트롤러 생성  2개의 메소트 index and show 작성

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    // Show all listings
+    public function index() {
+        
+    }
+
+    // Show single listing
+    public function show() {
+
+    }
+}
