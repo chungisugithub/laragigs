350ff6da58f544f67a07df319a10b8c0974be0c4
chungisugithub <chungisu@gmail.com>
2022-11-22 04:47:33 +0900

    @csrf

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -9,6 +9,7 @@
       </header>
 
       <form method="post" action="/listings">
+        @csrf
           <div class="mb-6">
               <label for="company" class="inline-block text-lg mb-2">Company Name</label>
               <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company">
