d7c05c23ea6187bef948d666211801b64901ad4f
chungisugithub <chungisu@gmail.com>
2022-11-23 04:08:30 +0900

    get() 덤프

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -12,7 +12,7 @@ class ListingController extends Controller
     public function index() {
         dd(Listing::latest()
         ->filter(request(['tag', 'search']))
-        ->paginate(2));
+        ->get());
         
         return view('listings.index', [
             'listings' => Listing::latest()
