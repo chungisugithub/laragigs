a8ffa76aab84112c61f7a690561a35ab84afb37c
chungisugithub <chungisu@gmail.com>
2022-11-23 04:02:15 +0900

    페이지 처리 get()을 paginate()로 변경

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -12,7 +12,8 @@ class ListingController extends Controller
     public function index() {
         return view('listings.index', [
             'listings' => Listing::latest()
-            ->filter(request(['tag', 'search']))->get()
+            ->filter(request(['tag', 'search']))
+            ->paginate()
         ]);
     }
 
