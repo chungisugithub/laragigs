b167f5bca2d5e5481939fbb7f8bba005e8be164e
chungisugithub <chungisu@gmail.com>
2022-11-23 03:38:51 +0900

    회사 필드에 이전 입력값이 남아 있도록 입력 태그에 value 속성 추가

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -12,7 +12,8 @@
         @csrf
         <div class="mb-6">
           <label for="company" class="inline-block text-lg mb-2">Company Name</label>
-          <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company">
+          <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
+          value="{{ old('company') }}">
           @error('company')
             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
           @enderror
