5efb7b041280aa4189e59479ef1fc35c61b577e5
chungisugithub <chungisu@gmail.com>
2022-11-23 07:39:08 +0900

    현재값 표시

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -14,7 +14,7 @@
         <div class="mb-6">
           <label for="company" class="inline-block text-lg mb-2">Company Name</label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
-            value="{{ old('company') }}">
+            value="{{ $listing->company }}">
           @error('company')
             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
           @enderror
