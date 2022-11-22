820b2272b7b37f5030a648e07dace1b88ae50e7a
chungisugithub <chungisu@gmail.com>
2022-11-23 07:36:43 +0900

    업데이트 폼 작성

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -3,13 +3,14 @@
     <x-card class="p-10 max-w-lg mx-auto mt-24">
       <header class="text-center">
         <h2 class="text-2xl font-bold uppercase mb-1">
-          Create a Gig
+          Edit a Gig
         </h2>
-        <p class="mb-4">Post a gig to find a developer</p>
+        <p class="mb-4">Edit: {{ $listing->title }}</p>
       </header>
 
-      <form method="post" action="/listings" enctype="multipart/form-data">
+      <form method="POST" action="/listings/{{ $listing->id }}" enctype="multipart/form-data">
         @csrf
+        @method('PUT')
         <div class="mb-6">
           <label for="company" class="inline-block text-lg mb-2">Company Name</label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
