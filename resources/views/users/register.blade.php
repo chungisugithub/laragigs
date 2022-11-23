ee92232be6b4a41097d6872c536eec83bee9df81
chungisugithub <chungisu@gmail.com>
2022-11-24 03:51:06 +0900

    오류처리시 입력내용 보존 old() 헬프 사용

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -14,7 +14,8 @@
                 <label for="name" class="inline-block text-lg mb-2">
                     Name
                 </label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name">
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" 
+                name="name" value="{{ old('name') }}">
                 @error('name')
                   <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
