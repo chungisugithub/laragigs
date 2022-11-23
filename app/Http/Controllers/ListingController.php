52a09882c7360dd323aaa26d9901698e8efbb2fc
chungisugithub <chungisu@gmail.com>
2022-11-23 15:26:01 +0900

    리스팅 삭제 컨트롤러 메서드 작성

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,72 +9,89 @@ use Illuminate\Validation\Rule;
 class ListingController extends Controller
 {
     // Show all listings
-    public function index() {
+    public function index()
+    {
         return view('listings.index', [
             'listings' => Listing::latest()
-            ->filter(request(['tag', 'search']))
-            ->paginate(6)
+                ->filter(request(['tag', 'search']))
+                ->paginate(6)
         ]);
     }
 
     // Show single listing
-    public function show(Listing $listing) {
+    public function show(Listing $listing)
+    {
         return view('listings.show', [
             'listing' => $listing
         ]);
     }
 
     // Show create Form
-    public function create() {
+    public function create()
+    {
         return view('listings.create');
     }
 
     // Store Listing Data
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $formFields = $request->validate([
             'title' => 'required',
-            'company'=> ['required', Rule::unique('listings','company')],
-            'location'=> 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
             'website' => 'required',
             'email' => ['required', 'email'],
             'tags' => 'required',
             'description' => 'required'
         ]);
 
-        if($request->hasFile('logo')) {
-            $formFields['logo'] = $request->file('logo')->store('logos', 
-            'public');
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store(
+                'logos',
+                'public'
+            );
         }
-        
+
         Listing::create($formFields);
-        
+
         return redirect('/')->with('message', 'Listing created successfully!');
     }
 
     // Show Edit Form
-    public function edit(Listing $listing) {
+    public function edit(Listing $listing)
+    {
         return view('listings.edit', ['listing' => $listing]);
     }
 
-        // Update Listing Data
-        public function update(Request $request, Listing $listing) {
-            $formFields = $request->validate([
-                'title' => 'required',
-                'company'=> ['required'],
-                'location'=> 'required',
-                'website' => 'required',
-                'email' => ['required', 'email'],
-                'tags' => 'required',
-                'description' => 'required'
-            ]);
-    
-            if($request->hasFile('logo')) {
-                $formFields['logo'] = $request->file('logo')->store('logos', 
-                'public');
-            }
-            
-            $listing->update($formFields);
-            
-            return back()->with('message', 'Listing updated successfully!');
+    // Update Listing Data
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store(
+                'logos',
+                'public'
+            );
         }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
+    }
+
+    // Delete Listing
+    public function destory(Listing $listing)
+    {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing Deleted Successfully');
+    }
 }
