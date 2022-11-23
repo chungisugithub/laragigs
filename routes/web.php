afce13119ea777674e6dae6f7caab9e9f7aa46f7
chungisugithub <chungisu@gmail.com>
2022-11-23 18:52:34 +0900

    사용자 저장 라우터 작성

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,6 @@ Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 // Show Register/Create Form
 Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
