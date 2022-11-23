6acd94923256b952417a9e69c4b5e3ae90f3b1e0
chungisugithub <chungisu@gmail.com>
2022-11-23 15:22:17 +0900

    삭제 라우터 작성

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,9 @@ Route::get('/listings/{listing}/edit',[ListingController::class, 'edit']);
 // Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destory']);
+
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
