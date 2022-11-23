19ac50e10d203c658bdcbe55407edc6376ac0fa8
chungisugithub <chungisu@gmail.com>
2022-11-24 03:50:03 +0900

    패스워드 검증2 'password' => 'required|confirmed|min:5'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,8 @@ class UserController extends Controller
         $formFields = $request->validate([
             'name' => ['required', 'min:3'],
             'email' => ['required', 'email', Rule::unique('users','email')],
-            'password' => ['required', 'confirmed', 'min:5']
+            // 'password' => ['required', 'confirmed', 'min:5'] // 또는 아래
+            'password' => 'required|confirmed|min:5'
         ]);
 
         // Hash Password
