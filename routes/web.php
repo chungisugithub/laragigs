97912b7f0922e454bd3967c432ea9f1f3c7460e5
chungisugithub <chungisu@gmail.com>
2022-11-24 08:07:32 +0900

    로그인 페이지도 게스트만

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,8 @@ Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
 // Show Register/Create Form
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])
+->middleware('guest');
 
 // Create New User
 Route::post('/users', [UserController::class, 'store']);
@@ -66,7 +67,7 @@ Route::post('/logout', [UserController::class, 'logout'])
 
 // Show Login Form
 Route::get('/login', [UserController::class, 'login'])
-->name('login');
+->name('login')->middleware('guest');
 
 // Log In User
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
