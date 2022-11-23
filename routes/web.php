3734c12789640b7d63f7192c5ecd4a1773637986
chungisugithub <chungisu@gmail.com>
2022-11-24 07:00:37 +0900

    로그인 라우터

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,6 @@ Route::post('/logout', [UserController::class, 'logout']);
 
 // Show Login Form
 Route::get('/login', [UserController::class, 'login']);
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
