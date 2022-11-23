034f080d9f5a0b54a41e5952158363a20fe7bcb8
chungisugithub <chungisu@gmail.com>
2022-11-24 08:29:47 +0900

    사용자 1명, 리스트 6개 관계설정 php artisan migrate:refresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -14,8 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
+        $user = User::factory()->create([
+            'name'=>'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
 
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
