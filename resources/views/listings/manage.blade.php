693a5f75d3dda0e13d07318fd4b637d2960fdff6
chungisugithub <chungisu@gmail.com>
2022-11-24 16:37:54 +0900

    @foreach row

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -9,6 +9,7 @@
 
         <table class="w-full table-auto rounded-sm">
             <tbody>
+              @foreach ($listings as $listing)
                 <tr class="border-gray-300">
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
                         <a href="show.html">
@@ -28,26 +29,7 @@
                         </form>
                     </td>
                 </tr>
-
-                <tr class="border-gray-300">
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="show.html">
-                            Junior Developer Opening
-                        </a>
-                    </td>
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="edit.html" class="text-blue-400 px-6 py-2 rounded-xl"><i class="fa-solid fa-pen-to-square"></i>
-                            Edit</a>
-                    </td>
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <form action="">
-                            <button class="text-red-600">
-                                <i class="fa-solid fa-trash-can"></i>
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
+              @endforeach
             </tbody>
         </table>
     </x-card>
